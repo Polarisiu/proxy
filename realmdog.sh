4fdbea160204f7f23abb60434abd348fa1db818d
Polaris <fdyrwaas@outlook.com>
2025-09-25 23:29:14 +0800

    Update realmdog.sh

--- realmdog.sh
+++ realmdog.sh
@@ -9,9 +9,11 @@ menu() {
     echo -e "${GREEN}=== Realm 转发管理 ===${RESET}"
     echo -e "${GREEN}1) 国外安装 Realm 转发${RESET}"
     echo -e "${GREEN}2) 国内安装 Realm 转发${RESET}"
+    echo -e "${GREEN}3) 国外安装 端口流量狗${RESET}"
+    echo -e "${GREEN}4) 国内安装 端口流量狗${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     echo
-    read -p $'\033[32m请选择操作 (0-2): \033[0m' choice
+    read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
         1)
             echo -e "${GREEN}正在国外环境安装 Realm 转发...${RESET}"
@@ -21,6 +23,16 @@ menu() {
         2)
             echo -e "${GREEN}正在国内环境安装 Realm 转发...${RESET}"
             wget -qO- https://ghfast.top/https://raw.githubusercontent.com/zywe03/realm-xwPF/main/xwPF.sh | sudo bash -s install
+            pause
+            ;;
+        3)
+            echo -e "${GREEN}正在国外环境安装 端口流量狗...${RESET}"
+            wget -O port-traffic-dog.sh https://raw.githubusercontent.com/zywe03/realm-xwPF/main/port-traffic-dog.sh && chmod +x port-traffic-dog.sh && ./port-traffic-dog.sh
+            pause
+            ;;
+        4)
+            echo -e "${GREEN}正在国内环境安装 端口流量狗...${RESET}"
+            wget -O port-traffic-dog.sh https://ghfast.top/https://raw.githubusercontent.com/zywe03/realm-xwPF/main/port-traffic-dog.sh && chmod +x port-traffic-dog.sh && ./port-traffic-dog.sh
             pause
             ;;
         0)
